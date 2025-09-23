9e69891edd1ff8b91a647d4b6f241383623b108e
Daniel Teel <daniel.teel.2@us.af.mil>
2025-09-22 20:33:22 -0600

    AsD

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './main.css';
 import { createRoot } from 'react-dom/client'
 import { CssBaseline } from '@mui/material';
 import Index from './Index';
